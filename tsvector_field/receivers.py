c50f4d05209f3a88c11915ccdc7a91ba80bf5e94
Viliam Segeda <viliam.segeda@gmail.com>
2017-04-05 09:18:55 +0200

    inject data operations only once

--- tsvector_field/receivers.py
+++ tsvector_field/receivers.py
@@ -15,28 +15,29 @@ class _TriggerEditorOperation(Operation):
         super().database_backwards(app_label, DatabaseTriggerEditor(schema_editor), from_state, to_state)
 
 
-def inject_trigger_operations(plan=None, **kwargs):
+def inject_trigger_operations(app_config, plan=None, **kwargs):
 
     if plan is None:
         return
 
     for migration, backward in plan:
-
-        inserts = []
-        for index, operation in enumerate(migration.operations):
-            clsname, args, kwargs = operation.deconstruct()
-            if clsname in ('CreateModel', 'DeleteModel', 'AddField', 'RemoveField', 'AlterField'):
-                newop = type('TriggerEditorOperation', (_TriggerEditorOperation, operation.__class__), {})
-                if clsname in ('DeleteModel', 'RemoveField'):
-                    # inject operation ahead of the original as we
-                    # need to introspect the not-yet-deleted field
-                    inserts.append((index, newop(*args, **kwargs)))
-                else:
-                    # inject operation after the original as we
-                    # need table and column to be created first
-                    inserts.append((index + 1, newop(*args, **kwargs)))
-
-        for inserted, (index, operation) in enumerate(inserts):
-            migration.operations.insert(inserted + index, operation)
+        if migration.app_label == app_config.label:
+            
+            inserts = []
+            for index, operation in enumerate(migration.operations):
+                clsname, args, kwargs = operation.deconstruct()
+                if clsname in ('CreateModel', 'DeleteModel', 'AddField', 'RemoveField', 'AlterField'):
+                    newop = type('TriggerEditorOperation', (_TriggerEditorOperation, operation.__class__), {})
+                    if clsname in ('DeleteModel', 'RemoveField'):
+                        # inject operation ahead of the original as we
+                        # need to introspect the not-yet-deleted field
+                        inserts.append((index, newop(*args, **kwargs)))
+                    else:
+                        # inject operation after the original as we
+                        # need table and column to be created first
+                        inserts.append((index + 1, newop(*args, **kwargs)))
+    
+            for inserted, (index, operation) in enumerate(inserts):
+                migration.operations.insert(inserted + index, operation)
 
 
